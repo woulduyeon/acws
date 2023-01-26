f7535064197a7e236fd6bcce0657bb91b250a71c
woulduyeon <woulduyeon@gmail.com>
2023-01-26 17:04:56 +0900

    0126_logo_slide_test

--- acws/style/js/swiper.js
+++ acws/style/js/swiper.js
@@ -1,15 +1,15 @@
   // import Swiper JS
-  import Swiper from 'swiper';
+  import Swiper from '../swiper';
   // import Swiper styles
-  import '../swiper/css';
+  import '../swiper/swiper-bundle.css';
 
   const swiper = new Swiper(...);
   // core version + navigation, pagination modules:
   import Swiper, { Navigation, Pagination } from 'swiper';
   // import Swiper and modules styles
-  import '../swiper/css';
-  import '../swiper/css/navigation';
-  import '../swiper/css/pagination';
+  import '../swiper/core';
+  import '../swiper/modules';
+  import '../swiper/type';
 
   // init Swiper:
   const swiper = new Swiper('.swiper', {
@@ -17,11 +17,6 @@
     modules: [Navigation, Pagination],
     ...
   });
-  // import Swiper bundle with all modules installed
-  import Swiper from 'swiper/bundle';
-
-  // import styles bundle
-  import 'swiper/css/bundle';
 
   // init Swiper:
   const swiper = new Swiper(...);
