0636768dfdcf3478329a61be755f513183c45948
woulduyeon <woulduyeon@gmail.com>
2023-02-21 17:02:34 +0900

    0221_banner resizing

---
+++ acws/style/js/slideText.js
@@ -0,0 +1,20 @@
+// 첫번째 슬라이드
+$(document).ready(function () {
+  $("#slideDate1").text("2022.11.11(금) 10:00~12:00(KST)"),
+    $("#slideTitle1").text("초청 강연: 인종과 젠더"),
+    $("#slideSum1").text(
+      "Defining “Mixed Blood”: (Re)making Koreanness at the Crossroads of Race and Gender"
+    ),
+    // 두번째 슬라이드
+    $("#slideDate2").text("2022.11.11(금) 10:00~12:00(KST)"),
+    $("#slideTitle2").text("초청 강연: 인종과 젠더"),
+    $("#slideSum2").text(
+      "Defining “Mixed Blood”: (Re)making Koreanness at the Crossroads of Race and Gender"
+    ),
+    // 세번째 슬라이드
+    $("#slideDate3").text("2022.11.11(금) 10:00~12:00(KST)"),
+    $("#slideTitle3").text("초청 강연: 인종과 젠더"),
+    $("#slideSum3").text(
+      "Defining “Mixed Blood”: (Re)making Koreanness at the Crossroads of Race and Gender"
+    );
+});
